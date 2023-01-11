08aa10bf5f33799ca7bd4bcfc902528328e6aca2
bsimmonds28 <bsimmonds28@gmail.com>
2023-01-10 23:33:03 -0500

    Edited CSS and JS

--- develop/script.js
+++ develop/script.js
@@ -1,10 +1,12 @@
 var termContainerEl = document.querySelector('#user-form');
-var searchTermEl = document.querySelector('#username');
+var searchTermEl = document.querySelector('#search');
 var buttonEl = document.querySelector('.btn');
 var repoContainerEl = document.querySelector('#repos-container');
-var timeButtonsEl = document.querySelector('#language-buttons');
+var timeButtonsEl = document.querySelector('#time-period');
 var results = document.querySelector('#repos-container');
 var futureResults = document.querySelector('#future-weather');
+var currentResults = document.querySelector('#current-weather');
+var pastSearchContainer = document.querySelector('#past-search-container');
 var citiesSearchedArray = [];
 var searchTerm;
 var timeSelection;
@@ -14,22 +16,25 @@ var formSubmit = function (event) {
     event.preventDefault();
   
     searchTerm = searchTermEl.value.trim();
-  
-    if (searchTerm) {
-      var citiesSearchedTemp = 
-      {
-        city: searchTerm,
-      };
-      citiesSearchedArray.push(citiesSearchedTemp);
-      storeInput();
 
-      getCoordinates(searchTerm);
-      searchTermEl.textContent = '';
-    } else {
-      alert('Please enter a valid city.');
-    }
+    searchTermCoordinates(searchTerm);
+};
 
-  };
+var searchTermCoordinates = function (searchTerm) {
+  if (searchTerm) {
+    var citiesSearchedTemp = 
+    {
+      city: searchTerm,
+    };
+    citiesSearchedArray.push(citiesSearchedTemp);
+    storeInput();
+
+    getCoordinates(searchTerm);
+    searchTermEl.value = '';
+  } else {
+    alert('Please enter a valid city.');
+  }
+};
 
 buttonEl.addEventListener('click', formSubmit);
 
@@ -38,9 +43,7 @@ var buttonClickHandler = function (event) {
     timeSelection = event.target.getAttribute('data-language');
   
     if (timeSelection) {
-      //getCorrectTime(timeSelection);
-      repoContainerEl.textContent = '';
-      return timeSelection;
+      searchTermCoordinates(searchTerm);
     }
 };
 
@@ -59,7 +62,7 @@ function init(){
         citiesSearchedArray = citiesSearchedStored;
     };
     
-    //printResults();
+    printPastSearches();
 }
     
 init();
@@ -119,30 +122,127 @@ var displayRepos = function (details) {
     title.textContent = searchTerm;
     results.appendChild(title);
     
-    if (timeSelection = "current") {
-        var icon = details[0].list[0].weather[0].icon;
-        title.appendChild(icon);
+    if (timeSelection == "current") {
+        getCurrentWeather(details);
 
-        var date = document.createElement('h5');
-        var currentDate = dayjs().format('MM/DD/YYYY');
-        date.textContent = currentDate;
-        results.appendChild(date);
+    } else if (timeSelection == "future") {
+        getFutureWeather(details);
 
-
-    } else if (timeSelection = "future") {
-        /*
-        for (i = 0; i < 5; i++) {
-
-            futureResults.appendChild();
-        };
-        */
     } else {
-        
+        getCurrentWeather(details);
+        getFutureWeather(details);
     };
 
 };
 
-//Print past search terms to left column
+//Function to print current weather
+var getCurrentWeather = function (details) {
+    var date = document.createElement('h5');
+    var currentDate = dayjs().format('MM/DD/YYYY');
+    date.textContent = currentDate;
+    currentResults.appendChild(date);
 
+    //icon
+    var icon = document.createElement('img');
+    var iconValCode = details.list[0].weather[0].icon;
+    var iconVal = "http://openweathermap.org/img/wn/" + iconValCode + "@2x.png";
+    icon.setAttribute("src", iconVal);
+    currentResults.appendChild(icon);
+
+    //temperature
+    var temp = document.createElement('h6');
+    var tempVal = details.list[0].main.temp;
+    temp.textContent = "Temp: " + tempVal + "°F";
+    currentResults.appendChild(temp);
+
+    //humidity
+    var humidity = document.createElement('h6');
+    var humidityVal = details.list[0].main.humidity;
+    humidity.textContent = "Humidity: " + humidityVal + "%";
+    currentResults.appendChild(humidity);
+
+    //windspeed
+    var windspeed = document.createElement('h6');
+    var windspeedVal = details.list[0].wind.speed;
+    windspeed.textContent = "Wind: " + windspeedVal + " MPH";
+    currentResults.appendChild(windspeed);
+};
+
+//Function to print future weather
+var getFutureWeather = function (details) {
+  for (i = 8; i < 40; i++) {
+
+    for (var i = 0; i < 1; i++){
+    var date = document.createElement('h5');
+    var k = i - (i-1);
+    var todaysDate = dayjs().add(k, 'day');
+    var dateVal = todaysDate.format('MM/DD/YYYY');
+    date.textContent = dateVal;
+    futureResults.appendChild(date);
+    };
+
+    //icon
+    var icon = document.createElement('img');
+    var iconValCode = details.list[i].weather[0].icon;
+    var iconVal = "http://openweathermap.org/img/wn/" + iconValCode + "@2x.png";
+    icon.setAttribute("src", iconVal);
+    futureResults.appendChild(icon);
+
+    //temperature
+    var temp = document.createElement('h6');
+    var tempVal = details.list[i].main.temp;
+    temp.textContent = "Temp: " + tempVal + "°F";
+    futureResults.appendChild(temp);
+
+    //humidity
+    var humidity = document.createElement('h6');
+    var humidityVal = details.list[i].main.humidity;
+    humidity.textContent = "Humidity: " + humidityVal + "%";
+    futureResults.appendChild(humidity);
+
+    //windspeed
+    var windspeed = document.createElement('h6');
+    var windspeedVal = details.list[i].wind.speed;
+    windspeed.textContent = "Wind: " + windspeedVal + " MPH";
+    futureResults.appendChild(windspeed);
+
+    i = i + 7;
+  }
+};
+
+//Print past search terms to left column
+var citySearchedListItem;
+
+function printPastSearches() {
+  var pastSearchHeader = document.createElement("h3");
+  pastSearchHeader.textContent = "VIEW PAST SEARCHES";
+  pastSearchContainer.appendChild(pastSearchHeader);
+
+  for (var i = 0; i < citiesSearchedArray.length; i++) {
+      var lastCitySearched = citiesSearchedArray[i].city;
+
+      if (lastCitySearched) {
+        citySearchedListItem = document.createElement("p");
+        citySearchedListItem.textContent = lastCitySearched;
+        pastSearchContainer.appendChild(citySearchedListItem);
+        citySearchedListItem.setAttribute('data-language', lastCitySearched);
+      }
+  };
+};
 
 //Click on past search term and again presented with those results
+var pastSearchClickHandler = function (event) {
+  var citySearchId = event.target.getAttribute('data-language');
+
+  if (citySearchId) {
+      searchTerm = citySearchId;
+      searchTermCoordinates(searchTerm);
+  }
+};
+
+pastSearchContainer.addEventListener('click', pastSearchClickHandler);
+
+/*
+Not currently clearing printed data before showing new time selection
+Need function to clear printed data
+*/
